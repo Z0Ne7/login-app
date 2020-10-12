bcead51e23ca3c74d743130297b5f6f23eb045ce
Canh <canhnt.bhsoft@gmail.com>
2020-10-13 00:26:25 +0700

    add actionTypes.ts

---
+++ src/constants/actionTypes.ts
@@ -0,0 +1,5 @@
+export const FETCH_DATA = 'FETCH_DATA';
+export const REQUEST_LOGIN = 'REQUEST_LOGIN';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'REQUEST_LOGIN';
+export const LOGOUT = 'LOGOUT';
