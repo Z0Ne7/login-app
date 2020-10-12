3398d054a5ab64c43adbc60e4bda93ad4f240d92
Canh <canhnt.bhsoft@gmail.com>
2020-10-13 00:25:49 +0700

    delete ActionTypes.ts

--- src/constants/ActionTypes.ts
+++
@@ -1,5 +0,0 @@
-export const FETCH_DATA = 'FETCH_DATA';
-export const REQUEST_LOGIN = 'REQUEST_LOGIN';
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-export const LOGIN_FAIL = 'REQUEST_LOGIN';
-export const LOGOUT = 'LOGOUT';
